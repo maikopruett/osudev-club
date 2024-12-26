b112fcc9335c4bc3c4b7241680ecac669d6696b1
Maiko Pruett <maikopruett@gmail.com>
2024-12-26 14:25:37 -0800

    error with padding

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,16 +4,7 @@ import './Nav.css'
 export default function Nav() {
   return (
     <div className='navbar-container'>
-        <ul className='navbar'>
-            <div className='navbar-left'>
-                <li>Home</li>
-            </div>
-            <div className='navbar-right'>
-                <li>About</li>
-                <li id='joinus-btn'>Join Us</li>
-                <li>Projects</li>
-            </div>
-        </ul>
+        
     </div>
   )
 }
