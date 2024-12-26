198c7badd96368dea2f1f762132e774d9a9a91e4
Maiko Pruett <maikopruett@gmail.com>
2024-12-26 14:54:36 -0800

    proper navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,14 @@ import './Nav.css'
 export default function Nav() {
   return (
     <div className='navbar-container'>
-        
+        <ul className='navbar'>
+            <li>Home</li>
+            <div className='navbar-right'>
+                <li>Projects</li>
+                <li>Join Us</li>
+                <li>Members</li>
+            </div>
+        </ul>
     </div>
   )
 }
