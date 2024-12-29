2ed46c53a8f3ca68c5f0c390724e1e87831a1d39
Maiko Pruett <maikopruett@gmail.com>
2024-12-29 00:28:00 -0800

    Update Main.jsx

--- src/components/Pages/Main/Main.jsx
+++ src/components/Pages/Main/Main.jsx
@@ -4,7 +4,11 @@ import './Main.css'
 export default function Main() {
   return (
     <div className='main-container'>
-      
+      <h1 className="animated-logo">OSU Dev Club</h1>
+      <div className="logo-underline"></div>
+      <div>
+        
+      </div>
     </div>
   )
 }
