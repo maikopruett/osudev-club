18d43da63e0b7e4308cbfb83dc3b0944fbb1408a
Maiko Pruett <maikopruett@gmail.com>
2025-01-09 19:54:55 -0800

    Main page desktop styling
    Changed OSU Dev Club main page styling to look like the mobile version

--- src/components/Pages/Main/Main.jsx
+++ src/components/Pages/Main/Main.jsx
@@ -6,16 +6,10 @@ export default function Main() {
   return (
     <>
     <div className='main-container'>
-      <h1 className="animated-logo">OSU Dev Club</h1>
+      <h1 className="animated-logo"><span>OSU</span> Dev Club</h1>
       <div className="logo-underline"></div>
       <div className='hero-tagline'>
-        Where Innovation Meets Orange
-      </div>
-      <div className='hero-quote'>
-        <p>
-          "At OSU Dev Club, we move fast, break things, and build startups that matter. 
-          We're not just coding—we're shaping the future."
-        </p>
+        Where Innovation Meets <span>Orange</span>
       </div>
       <NavLink to={'/join-us'}>
         <button className='main-call-to-action-btn'>
